eb92eb52e4ca31f5c6df259f0ddfa0e17e76b590
disco <haaritsubaki@gmail.com>
2019-06-26 20:22:19 +0200

    initial commit

--- Day2Exercises/string_exercises.py
+++ Day2Exercises/string_exercises.py
@@ -1,12 +1,10 @@
 messages = "Witaj, Roman Glegoła \n"
+print(messages)
 
 mess1 = "Witaj"
 mess2 = "Roman"
 mess3 = "Glegoła"
-
-print(messages)
 print(f'{mess1}, {mess2} {mess3}')
 
 # komentarze hash
-
 quit()
