76299744950cb49549c68a30cc658069e0833ad5
disco <haaritsubaki@gmail.com>
2019-07-01 17:40:50 +0200

    Dodałem treść zadania channeling

---
+++ Day2Exercises/praca_domowa_challenging.py
@@ -0,0 +1,7 @@
+#Challenging:
+
+#5. Warunek if - przy pomocy metody input pobierz od uzytkownia wartosc temperatury. Ustaw 3 minimalne temperatury jako zmienne - zimna, ciepla, goraca (np. 10 stopni, 20 stopni, 30 stopni)
+#i na podstawie temperatury podanej przez uzytkownika, wyswietl czy jest zimno, cieplo, czy goraco
+
+#6. Petla while
+#Oblicz silnie z podanej przez uzytkownika (metoda input) liczby - wyszukaj algorytm na silnie i napisz - przydadza sie zmienne, warunek if, operator *= 
